d8bacc10e64b4b8b640e5598862aea65cd7f7051
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-07-04 18:59:36 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
+const express=require('express');
+const createError=require('http-errors')
+const bodyParser=require('body-parser');
+const db=require('./helper/db');
+const uploadimage=require('./Router/upload_image');
+const deleteimage=require('./Router/delete_image')
+const saveimage=require('./Router/upload_save.js')
+const path=require('path')
 
+const app=express();
+const authroute=require('./Router/authroute');
+
+
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json())
+
+// Calling Routes 
+app.use(authroute);
+app.use('/uploadimage',uploadimage);
+app.use(deleteimage)
+app.use(saveimage)
+
+
+const port=process.env.PORT||2000;
+
+//app.set('view engine','ejs');
+
+// for handling error 
+app.use(async(req,res,next)=>{
+   /* const error=new Error("Not Found");
+    error.status=404
+    next (error)*/
+    next(createError.NotFound("This route does not exits"))
+
+})
+
+app.use((err,req,res,next)=>{
+    res.status(err.status ||400)
+    res.send({
+        error:{
+            status:err.status ||400,
+            message: err.message
+        }
+    })
+})
+
+
+
+
+app.listen(port,function(){
+    console.log(`Server is Running on ${port}`);
+})
