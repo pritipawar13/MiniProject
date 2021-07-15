2d97e195dbeb45e43cd235a72a75c3ee6b6ecdb7
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-07-15 17:58:25 +0530

    Add files via upload

--- Router/authroute.js
+++ Router/authroute.js
@@ -1,20 +1,20 @@
+require('dotenv').config()
 const express=require('express');
 const router=require('express').Router()
 const bodyParser=require('body-parser');
-const app=express();
 const mongoose=require('mongoose');
+const jwt=require('jsonwebtoken');
 const multer=require('multer')
-const JWT=require('jsonwebtoken');
-
 const User=require('../model/user.model')
 const uploadModel=require('../model/upload.model')
 const createError=require('http-errors')
 const { authschema }=require('../helper/schema_validation.js');
 const path=require('path');
 const check_register=require('../Middleware/check_register');
-const {signAccessToken}=require('../helper/jwt_helper')
+const auth=require('../Middleware/check_register');
 //const uploaded_images=require('./upload_image')
 const maxfilesize=1024 *1024;
+ 
 
 router.post('/register',async(req,res,next)=>{    
     try{
@@ -40,30 +40,45 @@ router.post('/register',async(req,res,next)=>{
     }  
 })
 
-router.post('/login',async function (req,res,next){
-    try{
-        const result= await authschema.validateAsync(req.body)
-        const user=  await User.findOne({ email :result.email})
-        if(!user)  throw createError.NotFound('user not regesitered')
-            //res.send('user not regesitered')
+let  refreshTokens=[]
 
-        const ismatch= await user.isvalidpassword(result.password)
+function generateAccessToken(user){
+    return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn:'15m'})
+}
+
+function generateRefreshToken(user){
+    refreshToken= jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    return refreshToken
+}
+
+router.post('/login',async function (req,res){
+    try{
+        const result=  await authschema.validateAsync(req.body)
+        const user=  await  User.findOne({ email :result.email})
+        if(!user)
+            return res.status(400).send('user not regesitered')
+        const ismatch= await  user.isvalidpassword(result.password)
         if(!ismatch){
-            //res.send('password not valid')
-            throw createError.Unauthorized('username/password not valid')
+            return res.status(400).send('password not valid')
         }
-       // const accesstoken=await signAccessToken(user.id)
-        //res.send({accesstoken})
-       
-    res.send(` ${result.email} is Successfully Login`)
+        const userauth={email:result.email};
+        const accessToken=generateAccessToken(userauth)
+        const refreshToken=generateRefreshToken(userauth)
+        console.log(refreshTokens)
+        res.json({accessToken:accessToken,refreshToken:refreshToken})
     }catch(error){
-      //  throw error;
-       if(error.isJoi==true) 
-        return next(createError.BadRequest("Invalid Username/Password"))
-        next(error)
+      throw error;
     }
 })
 
+router.delete('/logout',function(req,res){
+    JWTtoken=req.body.token;
+    if(!JWTtoken) return res.send("Firstly done Login ")
+    refreshTokens=refreshTokens.filter(token=>token !=req.body.token)
+    console.log(refreshTokens)
+    res.send("sucessfully logout")
+})
 
 
 
@@ -78,4 +93,9 @@ router.get('/find-all',function(req,res){
     })
 })
 
+router.get('/find',auth,function(req,res){
+    User.find().then(rec => {
+        res.status(200).json(rec)
+      })
+    })
 module.exports=router;
---
+++ Router/password.js
@@ -0,0 +1,56 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const router=require('express').Router()
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt')
+const User=require('../model/user.model')
+const createError=require('http-errors')
+const { authschema }=require('../helper/schema_validation.js');
+const path=require('path');
+
+router.get('/password',function(req,res){
+    var email=req.body.email;
+    User.find({email:{$in:email}}).exec((err,data)=>{
+        if(err) return res.status(400).send("Email not found");
+        else{
+            res.json(data)
+        }
+    })
+})
+
+router.put('/forgot/password',function(req,res){
+    var email=req.body.email;
+    var newpassword=req.body.password;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(newpassword, salt);
+    User.update({email:email},{$set:{password:hash}}).exec((err,data)=>{
+        if(err) return res.status(400).send("Cannot be forgot Password");
+        else{
+           res.json({
+               status:200,
+               message:`${email} has been Successfully forgot the old Password`
+           })
+        }
+    })
+})
+
+router.get('/confirm/password',async function(req,res){
+    var email=req.body.email;
+    var password=req.body.password;
+    const salt =await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+   await bcrypt.compare(password,hash,function(err,data){
+        if(err) return res.status(400).send("Password is invalid Password")
+        else{
+            //res.status(200).send("Confirm the password")
+            res.json({
+                status:200,
+                message:`${email} has confirm the password`
+            })
+        }
+    })
+})
+
+
+
+module.exports=router
