8a83777bd70f3fa1459851ada9f8d032f7a9de1e
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-07-17 16:24:12 +0530

    Add files via upload

--- Router/delete_image.js
+++ Router/delete_image.js
@@ -3,12 +3,15 @@ const router=express.Router()
 const fs=require('fs');
 const bodyparser=require('body-parser');
 const path=require('path')
+const uploadModel=require('../model/upload.model');
+const imageData=uploadModel.find({})
 
 const port=process.env.PORT ||2000;
 
 const dirPath='./public/images'
 
 router.delete('/deleteimage/:imagename',function(req,res){
+    var image=req.params.imagename
     if(!req.params.imagename){
         console.log(" Error in the file . Image not found !!");
         message:" Error in deletion of image!!";
@@ -16,15 +19,18 @@ router.delete('/deleteimage/:imagename',function(req,res){
     }
     else{
         console.log("File received !!");
-        console.log(`image name : ${req.params.imagename}`)
-        
+        console.log(`image name : ${req.params.imagename}`)  
+        uploadModel.findOneAndDelete({image:req.params.imagename}).exec((err,images)=>{
+            if(err){
+              throw err;
+            }
+            console.log("image deleted from database;")
+        })      
             fs.unlink(dirPath+'/'+req.params.imagename,function(err){
                 if(err) throw err;
                 console.log(`successfully deleted ${req.params.imagename}`);
-            return res.status(200).send('Successfully! Image has been Deleted');
+                return res.status(200).send('Successfully! Image has been Deleted');
             })
-            
-
     }
 
 })
