a740384a9e5c41ed162b4cff7bb2eaf4bbfbe781
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-08-12 17:43:37 +0530

    Add files via upload

---
+++ helper/userdetails_validation.js
@@ -0,0 +1,15 @@
+const Joi=require('joi');
+
+const studentschema=Joi.object({
+     Firstname:Joi.string().required(),
+     Lastname:Joi.string().required(),
+     Email: Joi.string().email().lowercase().required(),
+     PhoneNumber:Joi.number().max(10).required(),
+     Address:Joi.string().min(2).max(100).required(),
+     ProfilePhoto:Joi.string().required(),
+     //Sign:Joi.string().required()
+});
+
+module.exports={
+    studentschema
+}
