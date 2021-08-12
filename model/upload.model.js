967a02ee05b70afb685d9be64e92805b578f6071
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-08-12 17:46:55 +0530

    Add files via upload

--- model/upload.model.js
+++ model/upload.model.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
 const Schema= mongoose.Schema;
 const connection=require('../helper/db');
+const Users=require('../model/user.model')
 
 const Uploadschema=new mongoose.Schema({
     description: String,
@@ -11,9 +12,13 @@ const Uploadschema=new mongoose.Schema({
     type: Date,
     default: new Date()
 },
-Path:String
-
+Path:String,
+user:{
+    type:mongoose.Types.ObjectId,
+    ref:'User'
+}
 })
+
 
 const uploadimage=mongoose.model('uploadimage',Uploadschema)
 module.exports=uploadimage
--- model/user.model.js
+++ model/user.model.js
@@ -1,15 +1,17 @@
 const mongoose=require('mongoose')
 const Joi=require('joi');
+const uniqueValidator = require('mongoose-unique-validator');
 const Schema= mongoose.Schema;
 const bcrypt=require('bcrypt')
-
+const uploadModel=require('./upload.model')
+const {ObjectId} = mongoose.Schema;
 
 const Userschema=new mongoose.Schema({
-    firstname:{
+firstname:{
     type:String,
     minlength:5,
     maxlenfth:50,
-    required:true
+    srequired:true
 },
 lastname:{
     type:String,
@@ -28,7 +30,17 @@ password:{
     minlength:5,
     maxlength:20,
     required:true
+},
+role:{
+    type:String,
+    default:"user",
+    enum:["user","admin","superadmin"]
 }
+/*images:[{
+        type:mongoose.Schema.Types.ObjectId,
+    ref:'uploadimage'
+    }]*/
+
 })
 // To set hash password and store that password into database.
 
@@ -46,7 +58,7 @@ Userschema.pre('save',async function(next){
 
 })
 
-Userschema.methods.isvalidpassword=async function(password){
+Userschema.methods.isvalidpassword= async function(password){
     try{
        return await bcrypt.compare(password,this.password)
 
@@ -54,7 +66,7 @@ Userschema.methods.isvalidpassword=async function(password){
         throw error
     }
 }
-
+Userschema.plugin(uniqueValidator);
 const User=mongoose.model('User',Userschema)
 module.exports=User
 
