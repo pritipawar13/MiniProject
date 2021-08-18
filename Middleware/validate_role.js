45daee92d36246b7309ca5c048d58d9b5adbf8f3
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-08-18 14:53:14 +0530

    Add files via upload

--- Middleware/validate_role.js
+++ Middleware/validate_role.js
@@ -1,44 +1,26 @@
 const jwt = require('jsonwebtoken');
-const User=require('../model/user.model')
 
+var adminauth = (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) return res.status(401).send('Access Denied: No Token Provided!');
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  if (decoded.role === 'admin') {
+    next();
+  }
+  return res.status(401).send('Access Denied: You dont have correct privilege to perform this operation');
+};
 
-var adminauth=function (req, res, next){
-    const token = req.headers['authorization'].split(' ')[1];
-    if (!token) return res.status(401).send('Access Denied: No Token Provided!');
+var superadminauth = (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) return res.status(401).send('Access Denied: No Token Provided!');
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  if (decoded.role === 'superadmin') {
+    next();
+  }
+  return res.status(401).send('Access Denied: You dont have correct privilege to perform this operation');
+};
 
-    const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-   if(decoded.role==='admin')
-    {
-        next();
-    }
-    else
-        return res.status(401).send('Access Denied: You dont have correct privilege to perform this operation');
-    
-}
-
-var superadminauth=function (req, res, next){
-    const token = req.headers['authorization'].split(' ')[1];
-    if (!token) return res.status(401).send('Access Denied: No Token Provided!');
-
-    const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-        if (err) {
-            return res.json({ success: false, message: 'Failed to authenticate token.' });
-        } else {
-            req.decoded = decoded;
-            next();
-        }
-    })
-/* if(decoded.role==='superadmin')
-    {
-        next();
-    }
-    else
-        return res.status(401).send('Access Denied: You dont have correct privilege to perform this operation');*/
-    
-}
-module.exports={
-    adminauth,
-    superadminauth
-}
-
-
+module.exports = {
+  adminauth,
+  superadminauth,
+};
