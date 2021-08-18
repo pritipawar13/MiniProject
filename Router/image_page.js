615747d3bb8ceedf776b21a07a011c05ef866953
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-08-18 14:51:23 +0530

    Add files via upload

--- Router/image_page.js
+++ Router/image_page.js
@@ -1,42 +1,36 @@
 // Paginantion : To show the limited number os pages in the database
 
-const express=require('express');
-const router=require('express').Router()
-const mongoose=require('../helper/db');
-const imageModel=require('../model/upload.model')
+const express = require('express');
 
-const port=process.env.PORT ||2005;
-router.get('/images',pagenateResult(imageModel),function(req,res){
-    res.json(res.pagenateResult)
-})
+const router = express.Router();
+const imageModel = require('../model/upload.model');
 
-function pagenateResult(imagemodel){
-    return async (req,res,next)=>{
-
-        const page=parseInt(req.query.page);
-        const limit=parseInt(req.query.limit);
-        const startindex=(page-1)*limit;
-        const endindex=page*limit;
-
-        const results={};
-    
-        results.next={
-            page:page+1,
-            limit:limit
-        }
-    
-        results.previous={
-            page:page-1,
-            limit:limit
-        }
-    try{
-        results.results=await imagemodel.find().limit(limit).skip(startindex).exec()
-        res.pagenateResult=results;
-        next()
-   }catch(err){
-       res.send(err)
+function pagenateResult(imagemodel) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const startindex = (page - 1) * limit;
+    const results = {};
+    results.next = {
+      page: page + 1,
+      limit,
+    };
+    results.previous = {
+      page: page - 1,
+      limit,
+    };
+    try {
+      results.results = await imagemodel.find().limit(limit).skip(startindex).exec();
+      res.pagenateResult = results;
+      next();
+    } catch (err) {
+      res.send(err);
     }
- }
+  };
 }
 
-module.exports=router
+router.get('/images', pagenateResult(imageModel), (req, res) => {
+  res.json(res.pagenateResult);
+});
+
+module.exports = router;
