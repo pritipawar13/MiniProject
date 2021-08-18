3351574f1d5492c59d6106e9502abb74d3e0042d
Priti RajendraPawar <73785568+pritipawar13@users.noreply.github.com>
2021-08-18 14:45:03 +0530

    Add files via upload

--- Router/report_image.js
+++ Router/report_image.js
@@ -1,139 +1,118 @@
-const express=require('express');
-const router=express.Router();
-const mongoose=require('mongoose')
-const jwt_decode = require('jwt-decode');
-const Reportimage=require('../model/report.model')
-const uploadModel=require('../model/upload.model');
+const express = require('express');
+
+const router = express.Router();
+const Reportimage = require('../model/report.model');
+const uploadModel = require('../model/upload.model');
 const User = require('../model/user.model');
-const {adminauth,superadminauth}=require('../Middleware/validate_role')
+const { adminauth, superadminauth } = require('../Middleware/validate_role');
 
-router.post('/report/image/:imageid',function(req,res){
-  var imageid=req.params.imageid
-    var reportuser=req.body.reportuser;
-    var reason=req.body.reason;
-    var isreport=req.body.report;s
-
-   if(isreport==false){
-        return res.status(200).json({
-            status:200,
-            message:"Image is statisfy all condition of authority..."
-        })
+router.post('/report/image/:imageid', (req, res) => {
+  try {
+    const { imageid } = req.params.imageid;
+    const { reportuser } = req.body.reportuser;
+    const { reason } = req.body.reason;
+    const isreport = req.body.report;
+    if (isreport == false) {
+      return res.status(200).json({
+        status: 200,
+        message: 'Image is statisfy all condition of authority...',
+      });
     }
-    const reportimage=new Reportimage({
-        imageid:imageid,
-        isreport:isreport,
-         reportuser:reportuser,
-         reason:reason
-      })
-  
-    reportimage.save(function(err,data){
-        if(err) throw err;
-        return res.status(200).json({
-            status:200,
-           message:"Reported image",
-           data:data
-        })
-    })
-})
+    const reportimage = new Reportimage({
+      imageid,
+      isreport,
+      reportuser,
+      reason,
+    });
+
+    reportimage.save().then((rec) => res.status(201).json(rec));
+  } catch (err) {
+    // throw err;
+    console.log(err);
+  }
+});
 
 // to see reported perticular images information with user information
 
-router.get('/perticular/image/report/:imageid',function(req,res){
-    Reportimage.find({imageid:req.params.imageid})
+router.get('/perticular/image/report/:imageid', (req, res) => {
+  Reportimage.find({ imageid: req.params.imageid })
     .populate({ path: 'posts', select: 'image describtion createdAt username filepath' })
-    .exec(function(err,result){
-      res.send(result)
-    })
-   })
+    .exec((err, result) => {
+      res.send(result);
+    });
+});
 
 // to see all reported images only by admins.
 
-router.get('/all-reported/images',adminauth,function(req,res){
-  Reportimage.find({}).exec((err,data)=>{
+router.get('/all-reported/images', adminauth, (req, res) => {
+  Reportimage.find({}).exec((err, data) => {
     res.status(200).json({
-      status:200,
-      sucess:true,
-      data:data
-    })
-  })
-})
+      status: 200,
+      sucess: true,
+      data,
+    });
+  });
+});
 
-// onle delete the image of reported image by admin 
- 
-router.delete('/delete/reported/images/by/admin/:imageid',adminauth,function(req,res){
- uploadModel.findByIdAndDelete({_id:req.params.imageid}).exec((err,data)=>{
-   return res.status(200).json({
-      status:200,
-      message:"Sucessfully deleted images"
-    })
+// onle delete the image of reported image by admin
 
-  })
-})
+router.delete('/delete/reported/images/by/admin/:imageid', adminauth, (req, res) => {
+  uploadModel.findByIdAndDelete({ _id: req.params.imageid }).exec((err, data) => res.status(200).json({
+    status: 200,
+    message: 'Sucessfully deleted images',
+  }));
+});
 
 // only superadmin can saw all information about user ,admin,superadmin
 
-router.get('/all/info/about/admin-user-superadmin/:role',superadminauth,function(req,res){
-    User.find({role:req.params.role}).exec((err,data)=>{
-     return res.status(200).json({
-        status:200,
-        message:`Detalis Information about ${req.params.role}`,
-        data:data
-      })
-    })
-    
-  })
+router.get('/all/info/about/admin-user-superadmin/:role', superadminauth, (req, res) => {
+  User.find({ role: req.params.role }).exec((err, data) => res.status(200).json({
+    status: 200,
+    message: `Detalis Information about ${req.params.role}`,
+    data,
+  }));
+});
 
 // only superadmin creates the new admins
 
-router.post('/create/new/admin',superadminauth,function(req,res){
-  var found=User.find({email:req.body.email})
-  if(!found){
-    const newadmin=new User({
-      firstname:req.body.firstname,
-      lastname:req.body.lastname,
-      email:req.body.email,
-      password:req.body.password,
-      role:req.body.role
-    })
-    newadmin.save()
+router.post('/create/new/admin', superadminauth, (req, res) => {
+  var found = User.find({ email: req.body.email });
+  if (!found) {
+    const newadmin = new User({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: req.body.password,
+      role: req.body.role,
+    });
+    newadmin.save();
     res.status(201).json({
-      message:`${req.body.email} has sucessfully created as Admin of the system`
-    })
-}
-else{
-  res.status(301).json({
-    message:`${req.body.email} has already found as a admin`
-  })
+      message: `${req.body.email} has sucessfully created as Admin of the system`,
+    });
+  } else {
+    res.status(301).json({
+      message: `${req.body.email} has already found as a admin`,
+    });
+  }
+});
 
-}
-  
-})
-
-
-
-
-router.get('/count/:imageid',function(req,res){
-  var imageid=req.params.imageid
-  Reportimgae.count({imageid:imageid},function(err, result) {
+router.get('/count/:imageid', (req, res) => {
+  var { imageid } = req.params;
+  Reportimage.count({ imageid }, (err, result) => {
     if (err) {
       console.log(err);
     } else {
-      res.json("Number of documents in the collection: " + result);
+      res.json(`Number of documents in the collection: ${result}`);
     }
   });
 });
 
+router.get('/all/report', (req, res) => {
+  Reportimage.find({}).exec((err, data) => res.status(200).json({
+    status: 200,
+    message: 'All report Images',
+    result: data,
+  }));
+});
 
-
-
-router.get('/all/report',function(req,res){
-    Reportimgae.find({}).exec((err,data)=>{
-        return res.status(200).json({
-            status:200,
-            message:"All report Images",
-            result:data
-        })
-    })
-})
-
-module.exports=router
+module.exports = router;
